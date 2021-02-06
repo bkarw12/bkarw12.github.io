16a6be71383602751cade4a77072c0332ae4595f
Bartłomiej Karwowski <bkarw12@gmail.com>
2021-02-06 20:52:26 +0100

    Change function(){} to _ => {}

--- scripts.js
+++ scripts.js
@@ -4,10 +4,10 @@ let GRID_COL = 5;
 let container = document.getElementById("container");
 let fields = new Array(GRID_ROW * GRID_COL);
 
-let updateFields = function() {
+let updateFields = () => {
     container.innerHTML = "";
-}
+};
 
-let randomizeFields = function(seed) {
-
-}
+let randomizeFields = seed => {
+    
+};
