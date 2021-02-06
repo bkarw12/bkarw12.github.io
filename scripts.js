bc0a6c23fdd01f3a8365bf9d75f2d866fcafba71
Bartłomiej Karwowski <bkarw12@gmail.com>
2021-02-06 20:44:42 +0100

    Add scripts template

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let GRID_ROW = 5;
+let GRID_COL = 5;
+
+let container = document.getElementById("container");
+let fields = new Array(GRID_ROW * GRID_COL);
+
+let updateFields = function() {
+    container.innerHTML = "";
+}
+
+let randomizeFields = function(seed) {
+
+}
