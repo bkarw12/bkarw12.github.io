d822e558775da826e821938c38ce91eab68aee0b
Bartłomiej Karwowski <bkarw12@gmail.com>
2021-02-07 12:12:02 +0100

    Add nextRange and nextFloat to RNG

--- scripts.js
+++ scripts.js
@@ -11,14 +11,11 @@ RNG = function(seed) {
 
     this.state = seed ? seed : Math.floor(Math.random() * (this.m - 1));
 }
-
 RNG.prototype.f = (x, a, c, m) => (x * a + c) % m;
-
 RNG.prototype.nextInt = function() {
     this.state = this.f(this.state, this.a, this.c, this.m);
     return this.state;
 }
-
 RNG.prototype.stringToSeed = function(seed) {
     let x = 1;
     for (let i = 0; i < seed.length; i++) {
@@ -26,6 +23,17 @@ RNG.prototype.stringToSeed = function(seed) {
     }
     return x;
 }
+RNG.prototype.nextFloat = function() {
+    // returns in range [0,1]
+    return this.nextInt() / (this.m - 1);
+  }
+  RNG.prototype.nextRange = function(start, end) {
+    // returns in range [start, end): including start, excluding end
+    // can't modulu nextInt because of weak randomness in lower bits
+    var rangeSize = end - start;
+    var randomUnder1 = this.nextInt() / this.m;
+    return start + Math.floor(randomUnder1 * rangeSize);
+  }
 
 
 let container = document.getElementById("container");
