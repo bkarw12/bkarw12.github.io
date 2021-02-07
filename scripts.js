ab3bd5f9061e548850839c9855b69e44f1cb9b83
Bartłomiej Karwowski <bkarw12@gmail.com>
2021-02-07 11:43:32 +0100

    Move RNG to scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,26 @@ let GRID_ROW = 5;
 let GRID_COL = 5;
 let SEED_LENGTH = 4;
 
+
+let RNG = seed => {
+    // LCG using GCC's constants
+    this.m = 0x80000000; // 2**31;
+    this.a = 1103515245;
+    this.c = 12345;
+
+    this.state = seed ? seed : Math.floor(Math.random() * (this.m - 1));
+}
+
+RNG.prototype.nextInt = () => {
+    this.state = (this.a * this.state + this.c) % this.m;
+    return this.state;
+}
+
+RNG.prototype.stringToSeed = () => {
+
+}
+
+
 let container = document.getElementById("container");
 let fields = new Array(GRID_ROW * GRID_COL);
 
