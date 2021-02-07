29cd737e8de26d3a1bcd8b03b693b7afe9d7c9df
Bartłomiej Karwowski <bkarw12@gmail.com>
2021-02-07 12:29:09 +0100

    Extract f and stringToSeed from RNG

--- scripts.js
+++ scripts.js
@@ -2,38 +2,38 @@ let GRID_ROW = 5;
 let GRID_COL = 5;
 let SEED_LENGTH = 4;
 
+// LCG using GCC's constants
+let M = 0x80000000; // 2**31;
+let A = 1103515245;
+let C = 12345;
 
-RNG = function(seed) {
-    // LCG using GCC's constants
-    this.m = 0x80000000; // 2**31;
-    this.a = 1103515245;
-    this.c = 12345;
-
-    this.state = seed ? seed : Math.floor(Math.random() * (this.m - 1));
-}
-RNG.prototype.f = (x, a, c, m) => (x * a + c) % m;
-RNG.prototype.nextInt = function() {
-    this.state = this.f(this.state, this.a, this.c, this.m);
-    return this.state;
-}
-RNG.prototype.stringToSeed = function(seed) {
+f = (x, a, c, m) => (x * A + C) % M;
+stringToSeed = function(seed) {
     let x = 1;
     for (let i = 0; i < seed.length; i++) {
-        x = this.f(x * seed.charCodeAt(i), this.a, this.c, this.m);
+        x = f(x * seed.charCodeAt(i));
     }
     return x;
 }
+
+RNG = function(seed) {
+    this.state = seed ? seed : Math.floor(Math.random() * (M - 1));
+}
+RNG.prototype.nextInt = function() {
+    this.state = f(this.state);
+    return this.state;
+}
 RNG.prototype.nextFloat = function() {
     // returns in range [0,1]
-    return this.nextInt() / (this.m - 1);
+    return this.nextInt() / (M - 1);
   }
-  RNG.prototype.nextRange = function(start, end) {
+RNG.prototype.nextRange = function(start, end) {
     // returns in range [start, end): including start, excluding end
     // can't modulu nextInt because of weak randomness in lower bits
     var rangeSize = end - start;
-    var randomUnder1 = this.nextInt() / this.m;
+    var randomUnder1 = this.nextInt() / M;
     return start + Math.floor(randomUnder1 * rangeSize);
-  }
+}
 
 
 let container = document.getElementById("container");
