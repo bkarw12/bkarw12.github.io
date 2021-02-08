06ba872550703a87545972038fc8de441dd70423
Bartłomiej Karwowski <bkarw12@gmail.com>
2021-02-08 19:12:19 +0100

    Refactor code

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@
 const GRID_ROW = 5;
 const GRID_COL = 5;
 const SEED_LENGTH = 4;
+const P1_G = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+const P2_G = [6, 7, 8, 9, 10, 11, 12, 13, 14];
+const P1_B = [14, 15, 16];
+const P2_B = [0, 16, 24];
 
 // LCG using GCC's constants
 const M = 0x80000000; // 2**31;
@@ -21,7 +25,7 @@ let stringToSeed = function(seed) {
         x = f(x * seed.charCodeAt(i));
     }
     return x;
-}
+};
 let getRandomSeed = function() {
     let getRandomLetter = () => String.fromCharCode(65 + Math.floor(Math.random() * 26));
 
@@ -36,29 +40,29 @@ let getRandomSeed = function() {
 ///////////////////////
 RNG = function(seed) {
     this.state = seed ? seed : Math.floor(Math.random() * (M - 1));
-}
+};
 RNG.prototype.nextInt = function() {
     this.state = f(this.state);
     return this.state;
-}
+};
 RNG.prototype.nextFloat = function() {
     // returns in range [0,1]
     return this.nextInt() / (M - 1);
-  }
+};
 RNG.prototype.nextRange = function(start, end) {
     // returns in range [start, end): including start, excluding end
     // can't modulu nextInt because of weak randomness in lower bits
     let rangeSize = end - start;
     let randomUnder1 = this.nextInt() / M;
     return start + Math.floor(randomUnder1 * rangeSize);
-}
+};
 /* Randomize array in-place using Durstenfeld shuffle algorithm */
 RNG.prototype.shuffleArray = function(arr) {
     for (let i = arr.length - 1; i > 0; i--) {
         let j = this.nextRange(0, i + 1);
         [arr[i], arr[j]] = [arr[i], arr[j]] = [arr[j], arr[i]];
     }
-}
+};
 
 
 ////////////////////////////////////////////
@@ -87,12 +91,16 @@ let randomizeFields = function(seed) {
     // P1 - 9xG, 5xN, 3xB, 8xN
     // P2 - 1xB, 5xN, 9xG, 1xN, 1xB, 7xN, 1xB
     // 0s are already assigned, we only need to do the 1s and 2s
-    for(let i = 0; i < 9; i++) {
-        fields1[ids[i]] = 1;
-        fields2[ids[i + 6]] = 1;
+    for (let i = 0; i < 9; i++) {
+        fields1[ids[P1_G[i]]] = 1;
+        fields2[ids[P2_G[i]]] = 1;
     }
-    fields1[ids[14]] = fields1[ids[15]] = fields1[ids[16]] = 2;
-    fields2[ids[0]] = fields2[ids[16]] = fields2[ids[24]] = 2;
+    for (let i = 0; i < 3; i++) {
+        fields1[ids[P1_B[i]]] = 2;
+        fields2[ids[P2_B[i]]] = 2;
+    }
+
+    updateFields();
 };
 
 let updateSeed = e => randomizeFields(e.target.value);
